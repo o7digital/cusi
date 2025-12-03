6ce953f68a62cc34019a40530151aacb452c5267
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-03 12:58:55 -0600

    Fix navigation JSX structure

--- components/headers/components/Nav.jsx
+++ components/headers/components/Nav.jsx
@@ -73,7 +73,6 @@ export default function Nav() {
           CUSI
         </Link>
       </li>
-      <li className="navigation__item">
       <li className="navigation__item navigation__item_has-sub">
         <Link
           href="/servicios"

c4193f738383d112fa28d8b8c459d3f9367ebdb5
o7digital <101292703+o7digital@users.noreply.github.com>
2026-01-16 16:09:40 -0600

    feat: refresh floral categories with new bouquets

--- data/topCategories.js
+++ data/topCategories.js
@@ -1,43 +1,97 @@
 export const topCategoriesFlorial = [
   {
     id: 1,
-    name: "10 de Mayo",
-    imageSrc: "/assets/images/home/florial/category-birthday.jpg",
+    name: "Pesame",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/11-2-scaled-e1768586115334.png",
     objectPosition: "center",
-    href: "/servicios/10-de-mayo",
+    href: "/servicios/pesame",
   },
   {
     id: 2,
-    name: "Cumpleaños",
-    imageSrc: "/assets/images/home/florial/category-birthday.jpg",
+    name: "Tulipanes",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/7-2-scaled-e1768584799214.png",
     objectPosition: "center",
-    href: "/servicios/cumpleanos",
+    href: "/shop-1",
   },
   {
     id: 3,
-    name: "Día de Muertos",
-    imageSrc: "/assets/images/home/florial/category-sympathy.jpg",
+    name: "Bodas",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/3-scaled-e1768583670295.png",
     objectPosition: "center",
-    href: "/servicios/dia-de-muertos",
+    href: "/servicios/bodas",
   },
   {
     id: 4,
-    name: "Navidad",
-    imageSrc: "/assets/images/home/florial/category-sympathy.jpg",
+    name: "Rosas",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/20-2-scaled-e1768590981299.png",
     objectPosition: "center",
-    href: "/servicios/navidad",
+    href: "/shop-1",
   },
   {
     id: 5,
-    name: "Regalo",
-    imageSrc: "/assets/images/home/florial/category-plant.jpg",
+    name: "Para Toda Ocasion",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/13-2-scaled-e1768586341663.png",
     objectPosition: "center",
     href: "/shop-1",
   },
   {
     id: 6,
-    name: "Promociones y ofertas",
-    imageSrc: "/assets/images/home/florial/category-wedding.jpg",
+    name: "Flores Semanales",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/8-2-scaled-e1768584884975.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 7,
+    name: "Catenda Rizatas",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/16-2-scaled-e1768586666779.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 8,
+    name: "Regalos",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/15-2-scaled-e1768586540435.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 9,
+    name: "Orquideas",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/18-2-1-scaled-e1768589537995.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 10,
+    name: "Multicolores",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/10-2-scaled-e1768585737334.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 11,
+    name: "Detalles",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/4-2-scaled-e1768584336319.png",
+    objectPosition: "center",
+    href: "/shop-1",
+  },
+  {
+    id: 12,
+    name: "Ramos",
+    imageSrc:
+      "https://oliviers45.sg-host.com/wp-content/uploads/2026/01/19-2-scaled-e1768590862212.png",
     objectPosition: "center",
     href: "/shop-1",
   },
